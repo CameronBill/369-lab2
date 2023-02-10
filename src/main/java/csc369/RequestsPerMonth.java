abb5863ed34b5f3ee3a6cec6844ff2292619773c
CameronBill <shrimpkicker@gmail.com>
2023-02-09 22:38:15 -0800

    Update RequestsPerMonth.java

--- src/main/java/csc369/RequestsPerMonth.java
+++ src/main/java/csc369/RequestsPerMonth.java
@@ -58,7 +58,7 @@ public class RequestsPerMonth {
 			    Context context) throws IOException, InterruptedException {
             int sum = 0;
             Text yearMonthText = new Text();
-            yearMonthText.set(toString(yearMonth.year) + "-" + YearMonthWritable.months[yearMonth.month.value - 1]);
+            yearMonthText.set(toString(yearMonth.year) + "-" + YearMonthWritable.months[yearMonth.month.value() - 1]);
             Iterator<IntWritable> itr = counts.iterator();
 
             while (itr.hasNext()){
