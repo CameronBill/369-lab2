67b763c5c1033dcd04aa8325863e2ba59655b47d
CameronBill <shrimpkicker@gmail.com>
2023-02-09 23:27:21 -0800

    Update YearMonthWritable.java

--- src/main/java/csc369/YearMonthWritable.java
+++ src/main/java/csc369/YearMonthWritable.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.WritableComparable;
-import org.apache.hadoop.io.DataInput;
-import org.apache.hadoop.io.DataOutput;
 import java.util.Comparator;
 
 public class YearMonthWritable implements WritableComparable<YearMonthWritable> {
@@ -16,13 +14,13 @@ public class YearMonthWritable implements WritableComparable<YearMonthWritable> 
     public static final String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
 
     @Override
-    public void readFields(DataInput in) throws IOException {
+    public void readFields(YearMonthWritable in) throws IOException {
         year = in.readUTF();
         month = in.readUTF();
     }
 
     @Override
-    public void write(DataOutput out) throws IOException {
+    public void write(YearMonthWritable out) throws IOException {
         out.writeUTF(year);
         out.writeUTF(month);
     }
